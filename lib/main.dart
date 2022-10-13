8cbab75113627784241dae819ef750b1eda99235
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-13 19:19:59 -0300

    feat: initialization auto firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:app_help_me/view/screen_add_project.dart';
 import 'package:app_help_me/view/screen_home.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
