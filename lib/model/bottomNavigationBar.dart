1682d976665b8755b8523e0a67528f7f96fa62fc
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-14 01:21:07 -0300

    fix: correct hidden body

--- lib/model/bottomNavigationBar.dart
+++ lib/model/bottomNavigationBar.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
 
 bottomNavigation() {
-  return(
-    Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      crossAxisAlignment: CrossAxisAlignment.end,
-      children: [
-        Padding(
-          padding: EdgeInsets.all(8.0),
-          child: IconButton(
-            onPressed: () {
-      
-            },
-            color: Colors.blue,
-            icon: const Icon(Icons.add),
-          ),
-      )],
-    )
+  return (
+    Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(
+              onPressed: () {
+        
+              },
+              color: Colors.blue,
+              icon: const Icon(Icons.add),
+            ),
+        )],
+      )
   );
 }
