1e9f15ae4ed9296600b51a4dc4df1b1fc13ed744
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-13 23:06:27 -0300

    feat: add content

--- lib/view/screen_add_project.dart
+++ lib/view/screen_add_project.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../model/button.dart';
+import '../model/textField.dart';
+
 class ScreenAddProject extends StatefulWidget {
   const ScreenAddProject({super.key});
 
@@ -8,10 +11,48 @@ class ScreenAddProject extends StatefulWidget {
 }
 
 class _ScreenAddProjectState extends State<ScreenAddProject> {
+  var txtNomePT = TextEditingController();
+  var txtNomeEN = TextEditingController();
+  var txtDescricaoPT = TextEditingController();
+  var txtDescricaoEN = TextEditingController();
+  var txtLink = TextEditingController();
+  var txtLinkImage = TextEditingController();
+  var txtSkills = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: const Text('Adicionar projeto'),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              TextFieldGeneral('Nome português', txtNomePT, context),
+              const SizedBox(height: 5,),
+              TextFieldGeneral('Nome inglês', txtNomeEN, context),
+              const SizedBox(height: 5,),
+              TextFieldGeneral('Descrição português', txtDescricaoPT, context),
+              const SizedBox(height: 5,),
+              TextFieldGeneral('Descrição português', txtDescricaoEN, context),
+              TextFieldGeneral('Link', txtLink, context),
+              const SizedBox(height: 5,),
+              TextFieldGeneral('Link image', txtLinkImage, context),
+              const SizedBox(height: 5,),
+              TextFieldGeneral('Skills', txtSkills, context),
+
+              const SizedBox(height: 10,),
+
+              button('Adicionar', context, 'home')
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
