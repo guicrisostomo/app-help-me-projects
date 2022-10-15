60ccf4da1ec5064eb09e823b2429276852ecb2aa
guicrisostomo <guilhermecrisostomo75@gmail.com>
2022-10-15 14:41:05 -0300

    feat: add list skill

--- lib/view/screen_add_project.dart
+++ lib/view/screen_add_project.dart
@@ -1,4 +1,6 @@
+import 'package:app_help_me/model/textFieldSkills.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import '../model/button.dart';
 import '../model/textField.dart';
@@ -18,6 +20,14 @@ class _ScreenAddProjectState extends State<ScreenAddProject> {
   var txtLink = TextEditingController();
   var txtLinkImage = TextEditingController();
   var txtSkills = TextEditingController();
+  List<String> listSkill = [];
+
+  @override
+  void initState() {
+    super.initState();
+    txtSkills.text = '';
+    listSkill = [];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,8 +54,49 @@ class _ScreenAddProjectState extends State<ScreenAddProject> {
               const SizedBox(height: 5,),
               TextFieldGeneral('Link image', txtLinkImage, context),
               const SizedBox(height: 5,),
-              TextFieldGeneral('Skills', txtSkills, context),
+              TextFieldSkills(rotulo: 'Skills', variavel: txtSkills, onChanged:(value) {
+                setState(() {
+                  txtSkills.text = value;
+                  listSkill = value.split(',');
+                });
+                txtSkills.selection = TextSelection.fromPosition(TextPosition(offset: value.length));
+              },),
+              
+              if(listSkill.isNotEmpty)
+                ListView.builder(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: listSkill.length,
+                  itemBuilder: (context, index){
+                    return Card(
+                      child:ListTile(
+                        leading: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: SvgPicture.network(listSkill[index], height: 50, width: 50,)
+                        ),
+                        title: Text(listSkill[index]),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () {
+                            setState(() {
+                              listSkill.removeAt(index);
+                              txtSkills.text = '';
 
+                              for (var skill in listSkill) {
+                                if (txtSkills.text != '') {
+                                    txtSkills.text = '${txtSkills.text},$skill';
+                                } else {
+                                    txtSkills.text = skill;
+                                }
+                              }
+                            });
+                          },
+                        ),
+                      )
+                    );
+                  }
+                ),
+                
               const SizedBox(height: 10,),
 
               button('Adicionar', context, 'home', 'Save')
